7add46d93286982d5fd938c0e14f6df5f79d7f00
Abed El Kareem Massarwa <abodi.msarwi@gmail.com>
2024-12-03 00:56:34 +0200

    last commit !

--- Assets/Scripts/GameOverOnTrigger2D.cs
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverOnTrigger2D : MonoBehaviour
@@ -7,16 +5,24 @@ public class GameOverOnTrigger2D : MonoBehaviour
     [Tooltip("Every object tagged with this tag will trigger game over")]
     [SerializeField] string triggeringTag;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
             Debug.Log("Game over!");
+
+            // For builds
             Application.Quit();
-            UnityEditor.EditorApplication.isPlaying = false;  // Error on editor 2021.3
+
+            // For Unity Editor
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
         }
     }
 
-    private void Update() {
-        /* Just to show the enabled checkbox in Editor */
+    private void Update()
+    {
+        // Just to show the enabled checkbox in Editor
     }
-
 }
